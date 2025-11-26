0ac7682b00206514a53170d6c9bb394f84cc65d2
Bella9693 <realbella9693@gmail.com>
2025-11-26 13:45:57 -0800

    completed exercise

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,36 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config()
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRouter from "./routes/product.routes";
+dotenv.config();
 
-// Create server
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// Connect to MongoDB and Start Server
+app.use("/products", productRouter);
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Server is running!");
+});
+
+// 404 fallback
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).send("Invalid route!");
+});
+
 const PORT = process.env.PORT || 3000;
+const CONN_STRING = process.env.DATABASE_URL;
+
+if (!PORT || !CONN_STRING) {
+  throw new Error("Missing port or connection string!");
+}
+
 mongoose
-  .connect("mongodb+srv://<db_user>:<db_password>@ciccc.o8yo3tc.mongodb.net/<db_name>?retryWrites=true&w=majority&appName=<cluster_name>")
+  .connect(CONN_STRING, { dbName: "store" })
   .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () =>
+      console.log(`Server is running on http://localhost:${PORT}`)
+    );
   })
-  .catch((err) => console.error('Failed to connect to MongoDB', err));
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
